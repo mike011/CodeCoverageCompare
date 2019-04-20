bbb0553cf176b8d8d3782ec8f180f07157b856ae
mike011 <mike_ch_1@hotmail.com>
2019-04-20 09:38:08 -0400

    first

---
+++ CodeCoverageCompare/CodeCoverageCompare/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeCoverageCompare
+//
+//  Created by Michael Charland on 2019-04-20.
+//  Copyright © 2019 charland. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
