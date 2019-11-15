5782df80c4cdbbd3ab85a37ddef890b66c4c1199
mike011 <mike_ch_1@hotmail.com>
2019-11-15 13:28:51 -0500

    progress

---
+++ CodeCoverageFramework/CodeCoverageFrameworkTests/RowTests.swift
@@ -0,0 +1,44 @@
+//
+//  RowTests.swift
+//  CodeCoverageFrameworkTests
+//
+//  Created by Michael Charland on 2019-11-15.
+//  Copyright © 2019 charland. All rights reserved.
+//
+
+@testable import CodeCoverageFramework
+import XCTest
+
+class RowTests: XCTestCase {
+
+    func testRowPRAllCovered() {
+        let row = Row(file: "file", develop: 0, pr: 1)
+        XCTAssertEqual(row.change, "💯")
+    }
+
+    func testRowPRNotCovered() {
+        let row = Row(file: "file", develop: 0, pr: 0)
+        XCTAssertEqual(row.change, "🚫")
+    }
+
+    func testRowPRCoverageDown() {
+        let row = Row(file: "file", develop: 30, pr: 25)
+        XCTAssertEqual(row.change, "👎")
+    }
+
+    func testRowPRCoverageUp() {
+        let row = Row(file: "file", develop: 25, pr: 30)
+        XCTAssertEqual(row.change, "👍")
+    }
+
+    func testIsTest() {
+        let row = Row(file: "Test", develop: 0, pr: 0)
+        XCTAssertTrue(row.test)
+    }
+
+    func testIsNotTest() {
+        let row = Row(file: "file", develop: 0, pr: 0)
+        XCTAssertFalse(row.test)
+    }
+
+}
