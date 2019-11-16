ee5d43a577c04210e21d04e3b96e587a2072405c
mike011 <mike_ch_1@hotmail.com>
2019-11-15 19:40:13 -0500

    brackets and quotes

--- example/create_compare.sh
+++ example/create_compare.sh
@@ -6,10 +6,11 @@ cd ./After/Example
 fastlane test
 cd ../../
 
-BEFORE=./Before/Example/fastlane/test_output/Example.test_result.xcresult
-AFTER=./After/Example/fastlane/test_output/Example.test_result.xcresult
-xcrun xccov view  --report $BEFORE --json > before.json
-xcrun xccov view  --report $AFTER --json > after.json
+BEFORE='./Before/Example/fastlane/test_output/Example (Before).test_result.xcresult'
+xcrun xccov view  --report "$BEFORE" --json > before.json
+
+AFTER='./After/Example/fastlane/test_output/Example (After).test_result.xcresult'
+xcrun xccov view  --report "$AFTER" --json > after.json
 
 EQUIVALENCE=/Users/michael/Documents/git/CodeCoverageCompare/example/Before,/Users/michael/Documents/git/CodeCoverageCompare/example/After
-xcrun xccov diff --path-equivalence $EQUIVALENCE --json $BEFORE $AFTER >> coverage.json
+xcrun xccov diff --path-equivalence $EQUIVALENCE --json "$BEFORE" "$AFTER" >> coverage.json
