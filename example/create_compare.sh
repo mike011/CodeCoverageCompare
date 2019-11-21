e0f09ca0772256116c8c566d2a67e6c2b4a0d4b9
mike011 <mike_ch_1@hotmail.com>
2019-11-21 08:20:59 -0500

    change location that coverage compare files are created in

--- example/create_compare.sh
+++ example/create_compare.sh
@@ -1,16 +1,16 @@
 cd ./Before/Example
-fastlane test
+bundle exec fastlane test
 cd ../../
 
 cd ./After/Example
-fastlane test
+bundle exec fastlane test
 cd ../../
 
 BEFORE='./Before/Example/fastlane/test_output/Example (Before).test_result.xcresult'
-xcrun xccov view  --report "$BEFORE" --json > before.json
+xcrun xccov view --report "$BEFORE" --json > './Before/Example/fastlane/test_output/coverage.json'
 
 AFTER='./After/Example/fastlane/test_output/Example (After).test_result.xcresult'
-xcrun xccov view  --report "$AFTER" --json > after.json
+xcrun xccov view --report "$AFTER" --json > './After/Example/fastlane/test_output/coverage.json'
 
 EQUIVALENCE=/Users/michael/Documents/git/CodeCoverageCompare/example/Before,/Users/michael/Documents/git/CodeCoverageCompare/example/After
 xcrun xccov diff --path-equivalence $EQUIVALENCE --json "$BEFORE" "$AFTER" >> coverage.json
