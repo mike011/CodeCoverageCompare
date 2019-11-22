43a187356089ee5745ca8f2d6224cdbe8f88d6c8
mike011 <mike_ch_1@hotmail.com>
2019-11-21 20:02:55 -0500

    formatting

--- example/create_example_output.sh
+++ example/create_example_output.sh
@@ -5,6 +5,6 @@ rm -rf ../docs/after
 mkdir ../docs/before
 mkdir ../docs/after
 
-cp -R ./Before/example/fastlane/test_output/slather/*.html ../docs/before
-cp -R ./After/example/fastlane/test_output/slather/*.html ../docs/after
+cp -R ./Before/example/fastlane/test_output/slather/ ../docs/before
+cp -R ./After/example/fastlane/test_output/slather/ ../docs/after
 cp -R ./After/example/fastlane/test_output/*.html ../docs/after
