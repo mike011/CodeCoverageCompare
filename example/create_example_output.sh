5ee3a80c3e9c92add036b5a50fe2ec119f967aaf
mike011 <mike_ch_1@hotmail.com>
2020-02-17 12:06:00 -0500

    make folder structure closer to server

--- example/create_example_output.sh
+++ example/create_example_output.sh
@@ -7,4 +7,4 @@ mkdir -p ../docs/after/slather
 
 cp -R ./Before/example/fastlane/test_output/slather/ ../docs/before/slather
 cp -R ./After/example/fastlane/test_output/slather/ ../docs/after/slather
-cp -R ./After/example/fastlane/test_output/*.html ../docs/after/slather
+cp -R ./After/example/fastlane/test_output/*.html ../docs/after/
