3a9113707fcd66ef3ad13e242d3531fd8f1c362e
mike011 <mike_ch_1@hotmail.com>
2020-02-17 11:59:52 -0500

    moved files to match what they do with builds

--- example/create_example_output.sh
+++ example/create_example_output.sh
@@ -2,9 +2,9 @@
 rm -rf ../docs/before
 rm -rf ../docs/after
 
-mkdir ../docs/before
-mkdir ../docs/after
+mkdir -p ../docs/before/slather
+mkdir -p ../docs/after/slather
 
-cp -R ./Before/example/fastlane/test_output/slather/ ../docs/before
-cp -R ./After/example/fastlane/test_output/slather/ ../docs/after
-cp -R ./After/example/fastlane/test_output/*.html ../docs/after
+cp -R ./Before/example/fastlane/test_output/slather/ ../docs/before/slather
+cp -R ./After/example/fastlane/test_output/slather/ ../docs/after/slather
+cp -R ./After/example/fastlane/test_output/*.html ../docs/after/slather
